bc4d6ac334753761b0e82627684ac4e320d23c64
Connor Ketcheson <connorketcheson01@gmail.com>
2022-11-15 15:10:36 +0900

    complete function to add new books

--- objects.js
+++ objects.js
@@ -10,10 +10,10 @@ function Book(title, author, pages, read) {
     }
 };
 
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "295 pages", "read");
-myLibrary.push(theHobbit);
+// const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "295 pages", "read");
+// myLibrary.push(theHobbit);
 
-console.log(theHobbit.author);
+// console.log(theHobbit.author);
 
 //show or hide inputs
 const bookForm = document.getElementById('bookForm');
@@ -38,10 +38,11 @@ addBook.addEventListener('click', () => {
     title = document.getElementById("addTitle").value;
     author = document.getElementById("addAuthor").value;
     pages = document.getElementById("addPages").value;
-    if (document.getElementById("readCheck").checked = true) {
-        read = "read"
-    } else if (document.getElementById("readCheck").unchecked = false) {
+    const readCheck = document.getElementById("readCheck");
+    if (readCheck.checked = false) {
         read = " "
+    } else if (readCheck.checked = true) {
+        read = "read"
     };
     addBookToLibrary(title, author, pages, read);
 });
@@ -49,47 +50,61 @@ addBook.addEventListener('click', () => {
 //push new object to array
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read) 
-    myLibrary.push(newBook.title)
+    myLibrary.push(newBook)
     displayObject(newBook);
+    console.log(newBook);
+    console.log(myLibrary);
 };
 
+//erase table and refresh with new data
 //display object in table
 function displayObject(newBook) {
-    //for (let i = 0; i < myLibrary.length; i++) {
+    // const rows = document.getElementsByClassName("bookRow");
+    // while(rows.length > 0){
+    //     rows[0].parentNode.removeChild(rows[0]);
+    // }
+
+    //for (let i = myLibrary.length - 1; i < myLibrary.length; i--) {
         const newRow = document.createElement("tr");
+        newRow.class = "bookRow"
         
         const newTitle = document.createElement("td");
-        newTitle.innerText = newBook.title;
+        newTitle.innerText = myLibrary[myLibrary.length - 1].title;
         newRow.appendChild(newTitle);
 
         const newAuthor = document.createElement("td");
-        newAuthor.innerText = newBook.author;
+        newAuthor.innerText = myLibrary[myLibrary.length - 1].author;
         newRow.appendChild(newAuthor);
 
         const newPages = document.createElement("td");
-        newPages.innerText = newBook.pages;
+        newPages.innerText = myLibrary[myLibrary.length - 1].pages;
         newRow.appendChild(newPages);
 
         const newRead = document.createElement("td");
-        newRead.innerText = newBook.read;
+        newRead.innerText = myLibrary[myLibrary.length - 1].read;
+        newRead.id = myLibrary.length - 1;
         newRow.appendChild(newRead);
 
         const finished = document.createElement("button");
-        finished.type = "button";
+        finished.type = "button"
         finished.innerHTML = "Finished"
         finished.className = "finishButton"
-        finished.id = newBook.title;
+        finished.id = myLibrary.length - 1;
         newRow.appendChild(finished);
+        finished.addEventListener('click', () => {
+            myLibrary[finished.id].read = "read"
+            newRead.innerText = myLibrary[finished.id].read;
+        });
         
         const table = document.getElementById("bookTable");
         table.appendChild(newRow);
     //}
 };
 
-const finishRead = document.querySelectorAll('finishButton');
-finishRead.addEventListener('click', () => {
-    //title.read = "read"
-    if (finishButton.id = title) {
-        title.read = "read"
-    }
-});
+// const finishRead = document.querySelectorAll('finishButton');
+// finishRead.addEventListener('click', () => {
+//     //title.read = "read"
+//     if (finishButton.id = title) {
+//         title.read = "read"
+//     }
+// });
